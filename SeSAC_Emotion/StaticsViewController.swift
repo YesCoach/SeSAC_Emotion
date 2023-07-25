23d5e3fed9acfd076cec1f2b8f3f6a5679ef4a30
YesCoach <sbsii1020@gmail.com>
2023-07-25 17:43:49 +0900

    feat: 통계보기 View UI 구현

---
+++ SeSAC_Emotion/StaticsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  StaticsViewController.swift
+//  SeSAC_Emotion
+//
+//  Created by 박태현 on 2023/07/25.
+//
+
+import UIKit
+
+final class StaticsViewController: UIViewController {
+
+    @IBOutlet var moodViewCollection: [UIView]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+}
+
+private extension StaticsViewController {
+    func configureUI() {
+        moodViewCollection.forEach {
+            $0.layer.cornerRadius = 10.0
+            // opacity 더 찾아보기
+            // $0.layer.opacity = 0.8
+        }
+    }
+}
