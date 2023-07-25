477d29bc95b20668cb64fe69f26a651ce3a02ff0
YesCoach <sbsii1020@gmail.com>
2023-07-25 19:29:49 +0900

    feat: Pull down Button 기능 적용

--- SeSAC_Emotion/WriteEmotionViewController.swift
+++ SeSAC_Emotion/WriteEmotionViewController.swift
@@ -9,18 +9,22 @@ import UIKit
 
 final class WriteEmotionViewController: UIViewController {
 
+    @IBOutlet var emotionButtonCollection: [UIButton]!
+
     private var data: [Emotion: Int] = [:]
+    private var upCount = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureData()
+        configureButtonMenu()
     }
 
     @IBAction func didEmotionButtonTouched(_ sender: UIButton) {
         if let emotion = Emotion(rawValue: sender.tag),
            let count = data[emotion] {
-            data[emotion] = count + 1
-            print("\(emotion.title) \(count + 1)점!!")
+            data[emotion] = count + upCount
+            print("\(emotion.title) \(count + upCount)점!!")
         }
     }
 }
@@ -29,6 +33,20 @@ private extension WriteEmotionViewController {
     func configureData() {
         Emotion.allCases.forEach {
             data[$0] = 0
+        }
+    }
+
+    func configureButtonMenu() {
+        let action = [
+            UIAction(title: "+1", handler: { _ in self.upCount = 1 }),
+            UIAction(title: "+5", handler: { _ in self.upCount = 5 }),
+            UIAction(title: "+10", handler: { _ in self.upCount = 10 })
+        ]
+        emotionButtonCollection.forEach {
+            $0.menu = UIMenu(
+                options: .displayInline,
+                children: action
+            )
         }
     }
 }
