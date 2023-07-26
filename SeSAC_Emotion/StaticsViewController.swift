6df3b3fcc1322a541784d053b0235aa99fdc581f
YesCoach <sbsii1020@gmail.com>
2023-07-26 15:26:54 +0900

    feat: 통계보기 View에 UserDefaults 데이터 적용 - 항목별 Label의 tag를 Emotion 타입과 활용해서 UserDefaults로부터 데이터 패칭 - UserDefaultsManager의 loadEmotionData 메서드 사용

--- SeSAC_Emotion/StaticsViewController.swift
+++ SeSAC_Emotion/StaticsViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 final class StaticsViewController: UIViewController {
 
     @IBOutlet var moodViewCollection: [UIView]!
+    @IBOutlet var emotionCountLabelCollection: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureData()
     }
 }
 
@@ -21,6 +23,15 @@ private extension StaticsViewController {
     func configureUI() {
         moodViewCollection.forEach {
             $0.layer.cornerRadius = 10.0
+        }
+    }
+
+    func configureData() {
+        emotionCountLabelCollection.forEach {
+            if let emotion = Emotion(rawValue: $0.tag) {
+                let count = UserDefaultsManager.shared.loadEmotionData(emotion)
+                $0.text = "\(count)점"
+            }
         }
     }
 }
